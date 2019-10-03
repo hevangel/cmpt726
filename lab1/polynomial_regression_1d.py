66e6ab6eca59b374db8fcc7e7c81c42475477d33
hevangel <git@hevangel.com>
2019-10-02 17:02:10 -0700

    make bias configurable

--- lab1/polynomial_regression_1d.py
+++ lab1/polynomial_regression_1d.py
@@ -13,6 +13,7 @@ x = values[:,7:]
 #x = a1.normalize_data(x)
 
 N_TRAIN = 100
+bias = 0
 
 w = {}
 tr_err = {}
@@ -27,8 +28,8 @@ for col in range(8,15+1):
     t_train = targets[0:N_TRAIN]
     t_test = targets[N_TRAIN:]
 
-    (w[col], tr_err[col]) = a1.linear_regression(x_train, t_train, 'polynomial', 0, 3, 1)
-    (t_est[col], te_err[col]) = a1.evaluate_regression(x_test, t_test, w[col], 'polynomial', 3, 1)
+    (w[col], tr_err[col]) = a1.linear_regression(x_train, t_train, 'polynomial', 0, 3, bias)
+    (t_est[col], te_err[col]) = a1.evaluate_regression(x_test, t_test, w[col], 'polynomial', 3, bias)
 
 # importlib.reload(a1)
 
