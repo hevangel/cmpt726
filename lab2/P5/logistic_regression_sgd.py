5093cf0112c6bcf8024550e3dab8825a33be2d80
horace chan <hevangel@users.noreply.github.com>
2019-10-17 08:22:09 -0700

    P5 work in progress

--- lab2/P5/logistic_regression_sgd.py
+++ lab2/P5/logistic_regression_sgd.py
@@ -6,6 +6,7 @@ import numpy as np
 import scipy.special as sps
 import matplotlib.pyplot as plt
 import assignment2 as a2
+import math
 import random
 
 # Maximum number of iterations.  Continue until this limit, or when error change is below tol.
@@ -37,6 +38,7 @@ for eta in etas:
 
   # Initialize w.
   w = np.array([0.1, 0, 0])
+  w_old = w
 
   # Error values over all iterations.
   e_all = []
@@ -44,13 +46,14 @@ for eta in etas:
   DATA_FIG = 1
 
   # Set up the slope-intercept figure
-  SI_FIG = 2
-  plt.figure(SI_FIG, figsize=(8.5, 6))
-  plt.rcParams.update({'font.size': 15})
-  plt.title('Separator in slope-intercept space')
-  plt.xlabel('slope')
-  plt.ylabel('intercept')
-  plt.axis([-5, 5, -10, 0])
+  #SI_FIG = 2
+  #plt.figure(SI_FIG, figsize=(8.5, 6))
+  #plt.rcParams.update({'font.size': 15})
+  #plt.title('Separator in slope-intercept space')
+  #plt.xlabel('slope')
+  #plt.ylabel('intercept')
+  #plt.axis([-5, 5, -10, 0])
+
 
   for iter in range(0, max_iter):
 
@@ -60,35 +63,47 @@ for eta in etas:
 
     y = sps.expit(np.dot(X[index], w))
 
-    # e is the error, negative log-likelihood (Eqn 4.90)
-    e = -np.multiply(t[index], np.log(y)) + np.multiply((1-t[index]), np.log(1-y))
+    if t[index] != round(y):
 
-    # Add this error to the end of error vector.
-    e_all.append(e)
+      # e is the error, negative log-likelihood (Eqn 4.90)
+      #e = -np.multiply(t[index], np.log(y)) + np.multiply((1-t[index]), np.log(1-y))
+      e = -(t[index]*math.log(y) + (1-t[index])*math.log(1-y))
 
-    # Gradient of the error, using Eqn 4.91
-    grad_e = np.multiply((y - t[index]), X[index].T)
+      # Add this error to the end of error vector.
+      e_all.append(e)
 
-    # Update w, *subtracting* a step in the error derivative since we're minimizing
-    w_old = w
-    w = w - eta*grad_e
+      # Gradient of the error, using Eqn 4.91
+      grad_e = np.multiply((y - t[index]), X[index].T)
+
+      # Update w, *subtracting* a step in the error derivative since we're minimizing
+      w_old = w
+      w = w - eta*grad_e
+    else:
+      e = 0
+      e_all.append(0)
 
     # Plot current separator and data.  Useful for interactive mode / debugging.
-    #plt.figure(DATA_FIG)
-    #plt.clf()
-    #plt.plot(X1[:,0],X1[:,1],'b.')
-    #plt.plot(X2[:,0],X2[:,1],'g.')
-    #a2.draw_sep(w)
-    #plt.axis([-5, 15, -10, 10])
-    #plt.show()
+    plt.figure(DATA_FIG)
+    plt.clf()
+    plt.plot(X1[:,0],X1[:,1],'b.')
+    plt.plot(X2[:,0],X2[:,1],'g.')
+    if t[index] == 0:
+      plt.plot(X[index,0], X[index,1], 'bx')
+    else:
+      plt.plot(X[index,0], X[index,1], 'gx')
+    a2.draw_sep(w)
+    a2.draw_sep(w_old, line='y-')
+    plt.axis([-5, 15, -10, 10])
+    plt.show()
 
     # Add next step of separator in m-b space.
-    plt.figure(SI_FIG)
-    a2.plot_mb(w, w_old)
+    #plt.figure(SI_FIG)
+    #a2.plot_mb(w, w_old)
     #plt.show()
 
     # Print some information.
-    print('epoch {0:d}, negative log-likelihood {1:.4f}, w={2}'.format(iter, e, w.T))
+    print('epoch {0:d}, y={1:.2f}, t={2}, negative log-likelihood {3:.4f}, w={4}'.format(iter, y, t[index], e, w.T))
+    temp = 1
 
     # Stop iterating if error doesn't change more than tol.
     #if iter > 0:
