115d40ed9da0829b9c0f60b96d5f25c735255591
horace chan <hevangel@users.noreply.github.com>
2019-10-24 00:09:17 -0700

    add xor nn example, from numpy to pytorch

---
+++ xor/xor_nn_numpy.py
@@ -0,0 +1,70 @@
+import numpy as np
+import random
+
+# Times to run
+epoch = 10000
+
+# There are 2 inputs
+inputLayerSize = 2
+
+# NN nodes
+hiddenLayerSize = 3
+
+# Only one output
+outputLayerSize = 1
+
+L = 0.1
+
+# There are 2 inputs for XOR
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+# The truth table of XOR
+# ANN just can learn from truly examples!!!
+#     (adjust the weight and bias to make output is getting close to target by input)
+Y = np.array([[0], [1], [1], [0]])
+
+
+def sigmod(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_deriv(x):
+    return x * (1 - x)
+
+
+Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
+Wz = np.random.uniform(size=(hiddenLayerSize, outputLayerSize))
+
+for i in range(epoch):
+    H = sigmod(np.dot(X, Wh))
+    Z = np.dot(H, Wz)
+    E = Y - Z
+    dZ = E * L
+    Wz += H.T.dot(dZ)
+    dH = dZ.dot(Wz.T) * sigmoid_deriv(H)
+    Wh += X.T.dot(dH)
+
+print("**************** error ****************")
+print(E)
+print("***************** output **************")
+print(Z)
+print("*************** weights ***************")
+print("input to hidden layer weights: ")
+print(Wh)
+print("hidden to output layer weights: ")
+print(Wz)
+
+# Test
+fail = 0
+for i in range(0,100):
+    x1 = random.randint(0,1)
+    x2 = random.randint(0,1)
+    y_ground_truth = x1 ^ x2
+    y_predict = np.dot(sigmod(np.dot(np.array([x1, x2]), Wh)), Wz)
+    print('ground truth:', y_ground_truth, 'predct:', y_predict )
+    if y_ground_truth != round(y_predict[0]):
+        fail += 1
+
+print('fail:', fail)
+
+
